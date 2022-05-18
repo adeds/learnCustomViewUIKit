8f2a871c2b72eedf30b65ac11eb78bdc1eb12c76
adesamp <ade@sampingan.co.id>
2022-05-18 16:58:25 +0700

    add button signIn

--- learnCustomViewUIKit/LoginViewController.swift
+++ learnCustomViewUIKit/LoginViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class LoginViewController: UIViewController {
     
     let loginView = LoginView()
+    let signInButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,16 +23,34 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     private func style(){
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8
+        signInButton.setTitle("Sign In", for: [])
+        signInButton.addTarget(self, action: #selector(signinTapped), for: .primaryActionTriggered)
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton)
         
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
         ])
+        
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
     }
 }
 
+extension LoginViewController {
+    @objc func signinTapped(sender: UIButton) {
+        
+    }
+}
